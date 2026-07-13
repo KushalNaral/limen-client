2d6cda9d459886620a5a2625ba52dff3743d171c
Kushal <kushal.naral@yenyasoft.com>
2026-07-13 10:29:52 +0545

    UPDATE : Added new docs route and API typing

--- app/api/hooks/base/index.ts
+++ app/api/hooks/base/index.ts
@@ -1,13 +1,18 @@
 import { useQuery, type UseQueryResult } from "@tanstack/react-query";
 import { baseAPI } from "@/api/endpoints/base";
-import type { BaseAPIResponse } from "@/api/types";
+import type { BaseAPIResponse, HealthResponse } from "@/api/types";
 
 export const keys = {
   base: ["base"],
   health: ["health"],
 };
 
-export const useHealth = () => {};
+export const useHealth = (): UseQueryResult<HealthResponse, Error> => {
+  return useQuery({
+    queryKey: keys.health,
+    queryFn: ({ signal }) => baseAPI.health(signal),
+  });
+};
 
 export const useBase = (): UseQueryResult<BaseAPIResponse, Error> => {
   return useQuery({
--- app/routes.ts
+++ app/routes.ts
@@ -6,7 +6,10 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  layout("layouts/DefaultLayout.tsx", [index("routes/home.tsx")]),
+  layout("layouts/DefaultLayout.tsx", [
+    index("routes/home.tsx"),
+    route("docs", "pages/docs/index.tsx"),
+  ]),
   layout("layouts/auth/NonAuthLayout.tsx", [
     route("/login", "./pages/auth/login.tsx"),
   ]),
